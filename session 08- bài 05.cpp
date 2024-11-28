6aaac029b4259a19c8478e5b766f4773f0c10c3b
quangduymanh <quangduymanh4@gmail.com>
2024-11-29 00:20:15 +0700

    Update session 08- bài 05.cpp

--- session 08- bài 05.cpp
+++ session 08- bài 05.cpp
@@ -1,10 +1,11 @@
 #include<stdio.h>
-int main(){
-	int a[3][3]={{1,2,3},{4,5,6},{7,8,9}};
-	int sum=0;
-	for(int j=0;j<3;j++){
-		sum+=a[0][j];
-	}
-	printf("tong cua hang tren la %d",sum);
-	return 0;
+int main() {
+    int a[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int sum = 0;
+    for (int i = 0; i < 3; i++) {
+        sum += a[0][i] + a[2][i];
+        if (i > 0 && i < 2) sum += a[i][0] + a[i][2];
+    }
+    printf("Tong duong bien: %d", sum);
+    return 0;
 }
