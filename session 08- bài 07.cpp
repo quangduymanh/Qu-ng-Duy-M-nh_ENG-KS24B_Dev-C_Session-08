86ff62805cf0f8e0766bdbc8482038ad187ab887
quangduymanh <quangduymanh4@gmail.com>
2024-11-29 00:22:04 +0700

    Update session 08- bài 07.cpp

--- session 08- bài 07.cpp
+++ session 08- bài 07.cpp
@@ -1,9 +1,14 @@
 #include<stdio.h>
 int main(){
-	int n=3;
-	int a[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+	int n=4;
+	int a[4][4]={{1,2,3,4},{5,6,7,8},{20,30,40,50},{60,70,80,90}};
+	int sum= 0;
+	printf("cac phan tu duong cheo la: \n");
 	for(int i=0;i<n;i++){
-		printf("so phan tu duong cheo la: %d",a[i][i]);	
+		printf("%d",a[i][i]);
+		sum +=a[i][i];
 	}
-    return 0;
+	printf("\n tong so phan tu duong cheo la: %d \n",sum);
+	
+	return 0;
 }
